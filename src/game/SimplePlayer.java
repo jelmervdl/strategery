2b9a5e4211fb880827d160040ade59a1cb7d60ca
Jelmer <jelmer@ikhoefgeen.nl>
2012-10-22 17:57:45 +0200

    Implement simple player that will play any move that he might win.

---
+++ src/game/SimplePlayer.java
@@ -0,0 +1,29 @@
+package game;
+
+import java.util.List;
+
+public class SimplePlayer extends Player
+{
+	public SimplePlayer(String name)
+	{
+		super(name);
+	}
+
+	public Move decide(List<Move> possibleMoves, GameState state)
+	{
+		Move bestMove = null;
+		int largestDiceDifference = -1; // so 7 vs 7 will still result in moves.
+
+		for (Move move : possibleMoves)
+		{
+			int diff = move.getAttackingCountry().dice - move.getDefendingCountry().dice;
+			if (diff > largestDiceDifference)
+			{
+				bestMove = move;
+				largestDiceDifference = diff;
+			}
+		}
+
+		return bestMove;
+	}
+}
