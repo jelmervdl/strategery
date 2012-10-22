f143d3c3c17e321c7fd36168cffd6161d22d3528
Peter-Jan <pj_haga@hotmail.com>
2012-10-22 14:33:40 +0200

    Functies en variabelen logisch hernoemd

--- src/td/TDLearning.java
+++ src/td/TDLearning.java
@@ -10,43 +10,52 @@ import td.Chance;
 
 class TDLearning
 {
-    public HashMap<Move, Double> getMoves(Player player, GameState state)
+    public HashMap<Move, Double> mapValueToMove(Player player, GameState state, List<Move> moves)
     {
+        // Initialize HashMap where the values get mapped to the possible moves
         HashMap<Move, Double> expectedValues = new HashMap<Move, Double>();        
         List<GameState> states = new Vector<GameState>();
-        List<Move> moves = state.generatePossibleMoves(player);
-
+       
+        // For each possible move calculate the expected value from the state resulting from that move
         for (Move move : moves)
             expectedValues.put(move, getExpectedValueState(state, move));
-            
+        
+        // Return the map of values mapped to moves    
         return expectedValues;           
     }
 
     public double getExpectedValueState(GameState state, Move move)
     {
+        
         int attackingEyes = move.attackingCountry.dice;
 	    int defendingEyes = move.defendingCountry.dice;
         double expectedValue = 0;
 
+        // Calculate the expected value on the chances of winning losing and playing a draw with the values of the states as their result
+
         //win
-        expectedValue += Chance.chanceTable(attackingEyes, defendingEyes) * calcValue(state.expectedState(move, 1));
+        expectedValue += Chance.chanceTable(attackingEyes, defendingEyes) * calcValueState(state.expectedState(move, 1));
         //draw
-        expectedValue += 1-Chance.chanceTable(attackingEyes, defendingEyes)-Chance.chanceTable(defendingEyes, attackingEyes) * calcValue(state.expectedState(move, 2));
+        double drawChance = 1-Chance.chanceTable(attackingEyes, defendingEyes)-Chance.chanceTable(defendingEyes, attackingEyes);
+        expectedValue += drawChance * calcValueState(state.expectedState(move, 2));
         //lose        
-        expectedValue += Chance.chanceTable(defendingEyes, attackingEyes) * calcValue(state.expectedState(move, 3));
+        expectedValue += Chance.chanceTable(defendingEyes, attackingEyes) * calcValueState(state.expectedState(move, 3));
         
         return expectedValue;    
     }
-    
+
+    public double calcValueState(GameState state)
+    {
+        //NN calculeert de value van een gamestate
+        return 0;
+    }
+
     public void adjustNetwork(Move move, Double expectedValue, GameState state)
     {
-        double rewardValue = getExpectedValueState(state, move);
-        double targetValue = rewardValue + expectedValue;
+        double rewardValue;
+        double targetValue;
         
         //trainNN(state, targetValue);
     }
-    public double calcValue(GameState state)
-    {
-        return 0;
-    }
+
 }
--- src/td/TDPlayer.java
+++ src/td/TDPlayer.java
@@ -20,9 +20,14 @@ public class TDPlayer extends Player
 	public Move decide(List<Move> possibleMoves, GameState state)
 	{
         HashMap<Move, Double> possibleStates = new HashMap<Move, Double>();		
-        possibleStates = td.getMoves(this, state);
-        Move move = selectAction(possibleStates);        
+        // Calculate the value of the states resulting from the possible moves.        
+        possibleStates = td.mapValueToMove(this, state, possibleMoves);
+        // From the list of values mapped to the moves select the best move according to policy
+        Move move = selectAction(possibleStates);
+        // Adjust the NN for the move it just did.        
         td.adjustNetwork(move, possibleStates.get(move), state);       
+        
+        // return the move that is to be executed
         return move;
     }
     public Move selectAction(HashMap<Move, Double> possibleStates)
