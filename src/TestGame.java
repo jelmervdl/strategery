460e4a281bd247ada26419723758fa24d9dbb792
Jelmer <jelmer@ikhoefgeen.nl>
2012-10-23 13:07:44 +0200

    Use random map generation.

--- src/TestGame.java
+++ src/TestGame.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import game.*;
 import ui.terminal.TerminalUI;
 import ui.terminal.TerminalPlayer;
-import map.MapReader;
+import map.MapGenerator;
 import td.TDPlayer;
 
 public class TestGame 
@@ -20,57 +20,46 @@ public class TestGame
 		players.add(new SimplePlayer("Simple c"));
 		players.add(new RandomPlayer("Random d"));
 
-		MapReader reader = new MapReader(players);
-
 		TerminalUI gui = new TerminalUI();
 
-		String path = args.length > 0
-			? args[0]
-			: "../maps/1.txt";
+		MapGenerator generator = new MapGenerator(players);
 
 		// Initialize scores table
 		final HashMap<Player, Integer> scores = new HashMap<Player, Integer>();
 		for (Player player : players)
 			scores.put(player, 0);
 
-		try {
-			List<Country> countries = reader.read(new File(path));
-		
-			GameState state = new GameState(countries);
+		for (int i = 0; i < 500; ++i)
+		{
+			// Generate a random map
+			GameState state = generator.generate(4, 3.5);
 
-			for (int i = 0; i < 500; ++i)
-			{
-				final Game game = new Game(players, state);
-				game.addEventListener(new GameEventAdapter() {
-					public void onTurnEnded(GameState state)
+			final Game game = new Game(players, state);
+			game.addEventListener(new GameEventAdapter() {
+				public void onTurnEnded(GameState state)
+				{
+					if (!state.getPlayers().contains(tdPlayer))
 					{
-						if (!state.getPlayers().contains(tdPlayer))
-						{
-							System.out.println("Stopping game because TDPlayer died");
-							game.stop();
-						}
+						System.out.println("Stopping game because TDPlayer died");
+						game.stop();
 					}
-					
-					public void onGameEnded(GameState state)
-					{
-						Player winner = state.getCountries().get(0).getPlayer();
-						scores.put(winner, scores.get(winner) + 1);
+				}
+				
+				public void onGameEnded(GameState state)
+				{
+					Player winner = state.getCountries().get(0).getPlayer();
+					scores.put(winner, scores.get(winner) + 1);
 
-						// System.out.println("Winner: " + winner);
-					}
-				});
-				// game.addEventListener(gui);
-				game.run();
-			}
+					// System.out.println("Winner: " + winner);
+				}
+			});
+			// game.addEventListener(gui);
+			game.run();
+		}
 
-			// Print total scores table
-			System.out.println("Total scores:");
-			for (Player player : players)
-				System.out.println("Player " + player + ": \t" + scores.get(player));
-		}
-		catch (IOException e) {
-			System.out.println("Could not read map: " + e.getMessage());
-			return;
-		}
+		// Print total scores table
+		System.out.println("Total scores:");
+		for (Player player : players)
+			System.out.println("Player " + player + ": \t" + scores.get(player));
 	}
 }
